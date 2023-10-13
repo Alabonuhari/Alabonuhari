e377d8e7df20189c0d0cabc5096ba9664918c1cb
Alabonuhari <147806750+Alabonuhari@users.noreply.github.com>
2023-10-13 14:35:18 +0530

    Create adddition.js

---
+++ adddition.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>harika</title>
+</head>
+<body>
+    <h1>harika</h1>
+    <script>
+        var x=4;
+        x+=3;
+        console.log(x);
+    </script>
+</body>
+</html>
